ec408f292dbeca469b043b241904511053d44c3b
Piotr Dzierzega <dzierzega.p@gmail.com>
2016-01-12 01:51:32 +0100

    xsrf-token problems

--- app/app.js
+++ app/app.js
@@ -2,24 +2,39 @@
 
 // Declare app level module which depends on views, and components
 angular.module('myApp', [
-  'ngRoute',
-  'ngAnimate',
-  'ngCookies',
-  'formly',
-  'formlyBootstrap',
-  'ui.bootstrap',
-  'mgo-angular-wizard',
-  'myApp.home',
-  'myApp.login',
-  'myApp.register',
-  'myApp.view2',
-  'myApp.offers'
+    'ngRoute',
+    'ngAnimate',
+    'ngCookies',
+    'formly',
+    'formlyBootstrap',
+    'ui.bootstrap',
+    'mgo-angular-wizard',
+    'myApp.home',
+    'myApp.login',
+    'myApp.register',
+    'myApp.view2',
+    'myApp.offers'
 ]).
-config(['$routeProvider','$httpProvider', function($routeProvider,$httpProvider) {
-  $routeProvider.otherwise({redirectTo: '/home'});
-  $httpProvider.defaults.xsrfCookieName = '_csrf';
-  $httpProvider.defaults.xsrfHeaderName = 'X-XSRF-TOKEN';
-  $httpProvider.defaults.withCredentials = true;
-}]).run(function($rootScope) {
+        config(['$routeProvider', '$httpProvider', '$cookiesProvider', function ($routeProvider, $httpProvider, $cookies) {
+                $routeProvider.otherwise({redirectTo: '/home'});
+//  $httpProvider.defaults.xsrfCookieName = '_csrf';
+//  $httpProvider.defaults.xsrfHeaderName = 'X-XSRF-TOKEN';
+//  $httpProvider.defaults.withCredentials = true;
+                $httpProvider.defaults.headers.common["X-Requested-With"] = 'XMLHttpRequest';
+                $httpProvider.defaults.xsrfHeaderName = "X-XSRF-TOKEN";
+                $httpProvider.defaults.xsrfCookieName = "XSRF-TOKEN";
+            }]).run(function ($rootScope, $http, $cookies) {
     $rootScope.headers = {};
+    $http.defaults.transformRequest.push(function (data, headers) {
+        // retrieve the injector instance for the PdfCutter module
+        //  var injector = angular.injector(['pdfcutter', 'ng']);
+
+        // attach the correct django CSRF Token for each ajax query
+        if ($cookies.get('XSRF-TOKEN')) {
+            $http.defaults.headers.common["X-XSRF-TOKEN"]= $cookies.get('XSRF-TOKEN');
+            headers()['X-XSRF-TOKEN'] = $cookies.get('XSRF-TOKEN');
+        }
+
+        return data;
+    });
 });
--- app/login/login.js
+++ app/login/login.js
@@ -8,61 +8,57 @@ angular.module('myApp.login', ['ngRoute'])
                     controller: 'LoginCtrl',
                     controllerAs: 'vm'
                 });
-                $httpProvider.defaults.headers.common["X-Requested-With"] = 'XMLHttpRequest';
-                
+
+
             }])
 
-        .controller('LoginCtrl', ["$rootScope", "$scope", "$http", "$location", function ($rootScope, $scope, $http, $location) {
+        .controller('LoginCtrl', ["$rootScope", "$scope", "$http", "$location", "$cookies", "LoginService", function ($rootScope, $scope, $http, $location, $cookies, LoginService) {
                 // co musi byc wysylane:
                 // authorization - credidentiale
                 // X-Auth-Token - z endpointa /token
                 //X-XSRF-TOKEN - to co dostajesz z serwera
-                var authenticate = function (credentials, callback) {
-                    $rootScope.headers.Authorization = "Basic "
-                                + btoa(credentials.username + ":" + credentials.password);
-                    var headers = credentials ? {Authorization: "Basic "
-                                + btoa(credentials.username + ":" + credentials.password)
-                    } : {};
+//                var authenticate = function (credentials, callback) {
+//                    $rootScope.headers.Authorization = "Basic "
+//                                + btoa(credentials.username + ":" + credentials.password);
+//                    var headers = credentials ? {Authorization: "Basic "
+//                                + btoa(credentials.username + ":" + credentials.password)
+//                    } : {};
+//
+//                    $http({
+//                        method: 'GET',
+//                        headers: $rootScope.headers,
+//                        url: 'http://localhost:8080/token'
+//                    }).success(function (data) {
+//                        if (data.token) {      
+//                                $rootScope.authenticated = true;
+//                                $rootScope.headers['X-Auth-Token'] = data.token;
+//                        } else {
+//                            $rootScope.authenticated = false;
+//                        }
+//                        //callback && callback();
+//                    }).error(function () {
+//                        $rootScope.authenticated = false;
+//                        callback && callback();
+//                    });
+//
+//                }
 
-                    $http({
-                        method: 'GET',
-                        headers: $rootScope.headers,
-                        url: 'http://localhost:8080/login/'
-                    }).success(function (data) {
-                        if (data.name) {
-                            $rootScope.authenticated = true;
-                            $http({
-                                method: 'GET',
-                                url: 'http://localhost:8080/token',
-                                headers: $rootScope.headers,                                
-                            }).success(function (response) {
-                                $rootScope.headers['X-Auth-Token'] = response.token;
-                            }).error(function (response) {
-                                alert('unable to get token');
-                            });
-                        } else {
-                            $rootScope.authenticated = false;
-                        }
-                        //callback && callback();
-                    }).error(function () {
-                        $rootScope.authenticated = false;
-                        callback && callback();
-                    });
 
-                }
-
-                authenticate();
                 $scope.credentials = {};
-                $scope.login = function () {
-                    authenticate($scope.credentials, function () {
-                        if ($rootScope.authenticated) {
-                            $location.path("/");
-                            $scope.error = false;
-                        } else {
-                            $location.path("/login");
-                            $scope.error = true;
-                        }
-                    });
+//                 authenticate();
+                $scope.login = function ($cookies) {
+                     LoginService.login($scope.credentials.username, $scope.credentials.password,$cookies).then(function (data) {
+                         console.log(data);
+                     });
+//                    authenticate($scope.credentials, function () {
+//                        if ($rootScope.authenticated) {
+//                            $location.path("/");
+//                            $scope.error = false;
+//                        } else {
+//                            $location.path("/login");
+//                            $scope.error = true;
+//                        }
+//                    });
                 };
 
 
@@ -74,5 +70,36 @@ angular.module('myApp.login', ['ngRoute'])
                         $rootScope.authenticated = false;
                     });
                 }
-            }]);
+            }]).factory('LoginService', function ($q, $http,$rootScope) {
+    var login = function (username, password) {
+        var defered = $q.defer();
+        $rootScope.headers.Authorization = "Basic "
+                + btoa(username + ":" + password);
+        $http({
+            method: 'GET',
+            headers: $rootScope.headers,
+            url: 'http://localhost:8080/token'
+        }).success(function (data, status, headers, config) {
+            $rootScope.headers["X-Auth-Token"] = data.token;
+            delete $rootScope.headers.Authorization;
+            $http({
+                method:'POST',
+                headers:$rootScope.headers,
+                url: 'http://localhost:8080/offer/'
+            }).success(function(data){
+                console.log(data);
+            }).error(function(data){
+                 console.log(data);
+            });
+            defered.resolve(data);
+        }).error(function (data, status, headers, config) {
+            defered.reject(data);
+        });
+        return defered.promise;
+    };
+    return {
+        login: login
+    };
+});
+;
 
